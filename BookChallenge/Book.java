d142fbaaf2317330fe82b985cbd815beb1c5b7ab
AdrianeDriane <dilaoaparre@gmail.com>
2024-09-26 20:48:24 +0800

    BookChallenge3: Intermediate

--- BookChallenge/Book.java
+++ BookChallenge/Book.java
@@ -9,12 +9,17 @@ public class Book {
         this.author = "Unknown";
     }
 
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
     void showBookInfo() {
         System.out.println(title + " by " + author);
     }
 
     public static void main(String[] args) {
-        Book a = new Book();
+        Book a = new Book("So Good They Can't Ignore You", "Cal Newport");
 
         a.showBookInfo();
     }
