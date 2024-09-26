7a61727c30277682f46983862c3992d587bffb5f
AdrianeDriane <dilaoaparre@gmail.com>
2024-09-26 13:58:34 +0800

    CookieFactory.java

--- CookieFactory/CookieFactory.java
+++ CookieFactory/CookieFactory.java
@@ -1,1 +1,16 @@
+package CookieFactory;
 
+public class CookieFactory {
+
+    public static void main(String[] a) {
+        CookieCutter starCookieCutter = new CookieCutter("star");
+        CookieCutter umbrellaCookieCutter = new CookieCutter("umbrella");
+
+        Cookie starCookie = starCookieCutter.cut("vanilla");
+        Cookie umbrellaCookie = umbrellaCookieCutter.cut("chocolate");
+
+        starCookie.describe();
+        System.out.println();
+        umbrellaCookie.describe();
+    }
+}
