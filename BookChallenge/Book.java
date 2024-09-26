426ea493800b8f8b46d17fc041bb83eb023b08d6
AdrianeDriane <dilaoaparre@gmail.com>
2024-09-26 20:53:29 +0800

    BookChallenge4: Advanced

--- BookChallenge/Book.java
+++ BookChallenge/Book.java
@@ -14,13 +14,20 @@ public class Book {
         this.author = author;
     }
 
+    Book(String title) {
+        this.title = title;
+        this.author = "Unknown";
+    }
+
     void showBookInfo() {
         System.out.println(title + " by " + author);
     }
 
     public static void main(String[] args) {
         Book a = new Book("So Good They Can't Ignore You", "Cal Newport");
+        Book b = new Book("Rich Dad Poor Dad");
 
         a.showBookInfo();
+        b.showBookInfo();
     }
 }
